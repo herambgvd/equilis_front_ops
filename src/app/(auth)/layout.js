5336d0040fea67a0aa2e8e4fb5ddf6bde449168d
herambmishra <herambmishra@geniusvision.in>
2025-04-15 11:16:59 +0530

    Auth/Home Setup

---
+++ src/app/(auth)/layout.js
@@ -0,0 +1,13 @@
+export const metadata = {
+    title: "LogIn - Equilis - Beyond connectivity, towards intelligence",
+    description: "Equilis - Beyond connectivity, towards intelligence",
+  };
+
+export default function AuthLayout({ children }) {
+    return (
+      <>
+       {children}
+      </>
+    );
+  }
+  
